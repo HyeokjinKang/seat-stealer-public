62b4c88750fcc68adc2b752cde1226f63a4b4aa2
Coupy <coupy1024@icloud.com>
2024-04-09 15:26:14 +0900

    feat(supervisor): client join feat support

--- supervisor/src/index.ts
+++ supervisor/src/index.ts
@@ -3,8 +3,17 @@ import { Server } from "socket.io";
 const io = new Server();
 
 io.on("connection", (socket) => {
-  socket.on('create', function (room) {
+  socket.on('create', (room) => {
     socket.join(room);
+  });
+
+  socket.on("join", (room) => {
+    console.log(io.sockets.adapter.rooms.get(room));
+    if (io.sockets.adapter.rooms.get(room)) {
+      socket.join(room);
+    } else {
+      socket.emit("error", "room not exist");
+    }
   });
 });
 
